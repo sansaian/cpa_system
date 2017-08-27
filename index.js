5ce72bc7a7ffd24e50210e0573c5b4729b8d252f
i.sakhankov <i.sakhankov@innosoft.pro>
2017-08-27 19:33:51 +0300

    app ready

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 /**
  * Created by isakhankov on 27.08.17.
  */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(3000);
